fd2280bef85fde54fac0d867d203760dec79dcaa
StivelBernal <brayan.bernalg@gmail.com>
2023-03-06 03:47:53 -0500

    Fix

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="links-footer">
-      <a href="https://github.com/StivelBernal/pruebaTecnica">Link del Repositorio</a>
-      <a href="https://docs.google.com/document/d/1fGimEJE3oKwCfCnmIP8wiL4onEDGDQpGaSi38uJJ5mc/edit#">Documentación</a>
+      <a href="https://github.com/StivelBernal/pruebaTecnica" target="_blank">Link del Repositorio</a>
+      <a href="https://docs.google.com/document/d/1fGimEJE3oKwCfCnmIP8wiL4onEDGDQpGaSi38uJJ5mc/edit#" target="_blank">Documentación</a>
     </div>
 </footer>
